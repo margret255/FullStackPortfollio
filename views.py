393b688c5be2dc63defb5bf2ed866822d790cb6d
Wangui Margaret <148498603+margret255@users.noreply.github.com>
2024-12-27 07:12:48 +0300

    Update views.py

--- views.py
+++ views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Home(request)
+  return render(request,"index.html")
+
+
+def about(request)
+  return render(request,"about.html")
+              
+def projects(requeat)
+  return render(request,"projects.html")
+
+
+def contact(request)
+  return render(request,"contact.html")
