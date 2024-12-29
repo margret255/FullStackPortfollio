1ecbd0837fc344021db0e3c8caec38277733a182
Wangui Margaret <148498603+margret255@users.noreply.github.com>
2024-12-29 17:01:25 +0300

    Updated views login function

--- views.py
+++ views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import user authentication
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -8,6 +11,22 @@ def Home(request)
 
 def about(request)
   return render(request,"about.html")
+
+  
+def login(request)
+if request.method=="POST"
+  username=request.POST["|username"]
+  password=request.POST["passqord"]
+  user=authenticate(request,user)
+if user is  not None:
+  login(request,user)
+  return redirect(home)
+else:
+  messages.succcess(requestm("An error ocurred please try again")
+  pass
+
+return render(request,"|login.html)
+  
               
 def projects(requeat)
   return render(request,"projects.html")
